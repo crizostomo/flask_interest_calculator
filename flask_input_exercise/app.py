3f9dedf8c7859f16251dd60a70b62fe2799fefde
crizostomo <diogo_crizostomo@hotmail.com>
2021-12-21 07:52:33 -0300

    readme and code have been updated

--- flask_input_exercise/app.py
+++ flask_input_exercise/app.py
@@ -14,7 +14,7 @@ def interest_calculator():
         principal = float(request.form.get('principal'))
         interest = float(request.form.get('interest'))/100
         time = float(request.form.get('time'))
-        total = float(principal * ((1 + interest) ** time))
+        total = float(principal * ((1 + interest/100) ** time))
         return '''
                 
                   <h1>The amount of money accumulated in {} year(s) is: R$ {:.2f}</h1>
@@ -29,6 +29,35 @@ def interest_calculator():
                <input type="submit" value="Calculate">
            </form>'''
 
+
+@app.route('/json-calculator', methods = ["POST", "GET"])
+def json_example():
+    request_data = request.get_json()
+
+    principal = None
+    interest = None
+    time = None
+    total = None
+
+    if request_data:
+        if 'principal' in request_data:
+            principal = float(request_data['principal'])
+
+        if 'interest' in request_data:
+            interest = float(request_data['interest'])
+
+        if 'time' in request_data:
+            time = float(request_data['time'])
+
+        total = float(principal * ((1 + interest/100) ** time))
+
+
+    return '''
+           The principal value is: {}
+           The interest value is: {}
+           The Amount of time is: {}
+           Based on these data, you are going to have R$ {} 
+           '''.format(principal, interest, time, total)
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
